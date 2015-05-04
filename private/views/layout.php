9edd044e5cc08f7b9398040eb246ea04ffd314fb
geakstr <geakstr@me.com>
2015-05-04 11:03:05 +0300

    IE compatible meta tag

--- private/views/layout.php
+++ private/views/layout.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php echo $title; ?> | Система выполнения абстрактных заказов</title>
   <link rel="stylesheet" href="/css/style.css" />
   <script src="/js/utils.js"></script>
