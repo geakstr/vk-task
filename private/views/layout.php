8ab223f9a70076695c395d6bc5b36687281bada7
geakstr <geakstr@me.com>
2015-05-04 11:19:40 +0300

    json2 lib for IE

--- private/views/layout.php
+++ private/views/layout.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php echo $title; ?> | Система выполнения абстрактных заказов</title>
   <link rel="stylesheet" href="/css/style.css" />
+  <script src="/js/json2.js"></script>
   <script src="/js/utils.js"></script>
 </head>
 <body>
